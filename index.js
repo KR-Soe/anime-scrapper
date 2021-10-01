fac70eb2a57894f6b268faee3516b16830c712e8
Sergio <soe.vera.a@gmail.com>
2021-10-01 00:06:54 -0300

    double scrapper with one process fetching in reverse the latest rows

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const { run } = require('./scrapper');
 
 const main = () => {
-  run();
+  const baseUrl = 'https://myanimelist.net/topanime.php?limit=@pageRange';
+  
+  run(baseUrl.replace('@pageRange', 0), 50);
+  reverseBot(baseUrl);
+}
+
+const reverseBot = url => {
+  const maxRange = 18850
+  const reverseUrl = url.replace('@pageRange', maxRange);
+  const jumpInterval = -50
+
+  run(reverseUrl, jumpInterval, maxRange);
 }
 
 main();
